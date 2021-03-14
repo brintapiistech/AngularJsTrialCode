f2222968bc4168bf2640f18e114e4599bd830390
brintapiistech <brinta.piistech@gmail.com>
2021-03-14 15:39:17 +0600

    stored procedure

---
+++ Database/FloatingPointImage_Procedures.sql
@@ -0,0 +1,340 @@
+﻿USE DFW_Live
+GO
+
+/****** Object:  StoredProcedure [dbo]..InsertFloatingPointImage    Script Date: 3/12/2021 11:42:24 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[InsertFloatingPointImage]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[InsertFloatingPointImage]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE InsertFloatingPointImage
+(
+	@Id int OUTPUT,
+	@FloatingPointId uniqueidentifier,
+	@ImageLocation nvarchar(max),
+	@ImageName nvarchar(500),
+	@CreatedBy uniqueidentifier,
+	@CreatedDate datetime
+)
+AS
+    INSERT INTO [dbo].[FloatingPointImage] 
+	(
+	[FloatingPointId],
+	[ImageLocation],
+	[ImageName],
+	[CreatedBy],
+	[CreatedDate]
+    ) 
+	VALUES 
+	(
+	@FloatingPointId,
+	@ImageLocation,
+	@ImageName,
+	@CreatedBy,
+	@CreatedDate
+    )
+	DECLARE @Err int
+	DECLARE @Result int
+
+	SET @Result = @@ROWCOUNT
+	SET @Err = @@ERROR 
+	If @Err <> 0 
+	BEGIN
+		SET @Id = -1
+		RETURN @Err
+	END
+	ELSE
+	BEGIN
+		If @Result = 1 
+		BEGIN
+			-- Everything is OK
+			SET @Id = @@IDENTITY
+		END
+		ELSE
+		BEGIN
+			SET @Id = -1
+			RETURN 0
+		END
+	END
+
+	RETURN @Id
+GO
+
+/****** Object:  StoredProcedure [dbo].UpdateFloatingPointImage    Script Date: 3/12/2021 11:42:24 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateFloatingPointImage]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[UpdateFloatingPointImage]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE UpdateFloatingPointImage
+(
+	@Id int,
+	@FloatingPointId uniqueidentifier,
+	@ImageLocation nvarchar(max),
+	@ImageName nvarchar(500),
+	@CreatedBy uniqueidentifier,
+	@CreatedDate datetime
+)
+AS
+    UPDATE [dbo].[FloatingPointImage] 
+	SET
+	[FloatingPointId] = @FloatingPointId,
+	[ImageLocation] = @ImageLocation,
+	[ImageName] = @ImageName,
+	[CreatedBy] = @CreatedBy,
+	[CreatedDate] = @CreatedDate
+	WHERE ( Id = @Id )
+
+	DECLARE @Err int
+	DECLARE @Result int
+	SET @Result = @@ROWCOUNT
+	SET @Err = @@ERROR 
+
+	If @Err <> 0 
+	BEGIN
+		SET @Result = -1
+	END
+
+	RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].DeleteFloatingPointImage    Script Date: 3/12/2021 11:42:24 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DeleteFloatingPointImage]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[DeleteFloatingPointImage]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE DeleteFloatingPointImage
+(
+	@Id int
+)
+AS
+	DELETE [dbo].[FloatingPointImage] 
+
+    WHERE ( Id = @Id )
+
+	DECLARE @Err int
+	DECLARE @Result int
+
+	SET @Result = @@ROWCOUNT
+	SET @Err = @@ERROR 
+
+	If @Err <> 0 
+	BEGIN
+		SET @Result = -1
+	END
+
+	RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].GetAllFloatingPointImage    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetAllFloatingPointImage]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetAllFloatingPointImage]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetAllFloatingPointImage
+AS
+	SELECT *		
+	FROM
+		[dbo].[FloatingPointImage]
+
+RETURN @@ROWCOUNT
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointImageById    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointImageById]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointImageById]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointImageById
+(
+	@Id int
+)
+AS
+	SELECT *		
+	FROM
+		[dbo].[FloatingPointImage]
+	WHERE ( Id = @Id )
+
+RETURN @@ROWCOUNT
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointImageMaximumId    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointImageMaximumId]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointImageMaximumId]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointImageMaximumId
+AS
+	DECLARE @Result int
+	SET @Result = 0
+	
+	SELECT @Result = MAX(Id) 		
+	FROM
+		[dbo].[FloatingPointImage]
+
+	If @Result > 0 
+		BEGIN
+			-- Everything is OK
+			RETURN @Result
+		END
+		ELSE
+		BEGIN
+			RETURN 0
+		END
+RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointImageRowCount    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointImageRowCount]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointImageRowCount]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointImageRowCount
+AS
+	DECLARE @Result int
+	SET @Result = 0
+	SELECT @Result = COUNT(*) 		
+	FROM
+		[dbo].[FloatingPointImage]
+		
+RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].GetPagedFloatingPointImage    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetPagedFloatingPointImage]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetPagedFloatingPointImage]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetPagedFloatingPointImage
+(
+	@TotalRows		int	OUTPUT,
+	@PageIndex	int,
+	@RowPerPage		int,
+	@WhereClause	nvarchar(4000),
+	@SortColumn		nvarchar(128),
+	@SortOrder		nvarchar(4)
+)
+AS
+BEGIN 
+
+SET @PageIndex = isnull(@PageIndex, -1)
+SET @RowPerPage = isnull(@RowPerPage, -1)
+SET @WhereClause = isnull(@WhereClause, '')
+SET @SortColumn = isnull(@SortColumn, '')
+SET @SortOrder = isnull(@SortOrder, '')
+SET @TotalRows = 0
+SET @RowPerPage = @RowPerPage -1
+DECLARE @SQL1 nvarchar(4000)
+DECLARE @SQL2 nvarchar(4000)
+
+IF (@WhereClause != '')
+BEGIN
+	SET @WhereClause = 'WHERE ' + char(13) + @WhereClause	
+END
+
+IF (@SortColumn != '')
+BEGIN
+	SET @SortColumn = 'ORDER BY ' + @SortColumn
+
+	IF (@SortOrder != '')
+	BEGIN
+		SET @SortColumn = @SortColumn + ' ' + @SortOrder
+	END
+END
+ELSE
+BEGIN
+	SET @SortColumn = @SortColumn + ' ORDER BY [Id] ASC'
+END
+
+SET @SQL1 = 'WITH FloatingPointImageEntries AS (
+			SELECT ROW_NUMBER() OVER ('+ @SortColumn +')AS Row,
+	[Id],
+	[FloatingPointId],
+	[ImageLocation],
+	[ImageName],
+	[CreatedBy],
+	[CreatedDate]
+				FROM 
+				[dbo].[FloatingPointImage]
+					'+ @WhereClause +'
+				)
+				SELECT 
+	[Id],
+	[FloatingPointId],
+	[ImageLocation],
+	[ImageName],
+	[CreatedBy],
+	[CreatedDate]
+				FROM 
+					FloatingPointImageEntries
+				WHERE 
+					Row between '+ CONVERT(nvarchar(10), (@PageIndex * @RowPerPage) + 1) +'And ('+ CONVERT(nvarchar(10), (@PageIndex * @RowPerPage) +@RowPerPage+ 1) +')'
+	
+
+SET @SQL2 =		' SELECT @TotalRows = COUNT(*) 
+				FROM 
+				[dbo].[FloatingPointImage] ' + @WhereClause
+								
+EXEC sp_executesql @SQL2, N'@TotalRows int output', @TotalRows = @TotalRows output
+
+EXEC sp_executesql @SQL1
+
+RETURN @@ROWCOUNT
+END
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointImageByQuery    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointImageByQuery]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointImageByQuery]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointImageByQuery
+(
+	@Query	nvarchar(4000)
+)
+AS
+BEGIN 
+
+SET @Query = isnull(@Query, '')
+DECLARE @SQL1 nvarchar(4000)
+
+IF (@Query != '')
+BEGIN
+	SET @Query = 'WHERE ' + char(13) + @Query	
+END
+
+SET @SQL1 =		'SELECT * 
+				FROM 
+				[dbo].[FloatingPointImage] ' + @Query
+								
+EXEC sp_executesql @SQL1
+
+RETURN @@ROWCOUNT
+END
+GO
+
---
+++ Database/FloatingPoint_Procedures.sql
@@ -0,0 +1,480 @@
+﻿USE DFW_Live
+GO
+
+/****** Object:  StoredProcedure [dbo]..InsertFloatingPoint    Script Date: 3/12/2021 11:42:24 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[InsertFloatingPoint]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[InsertFloatingPoint]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE InsertFloatingPoint
+(
+	@Id int OUTPUT,
+	@FloatingPointId uniqueidentifier,
+	@Name nvarchar(250),
+	@District nvarchar(100),
+	@City nvarchar(100),
+	@Lat nvarchar(100),
+	@Lng nvarchar(100),
+	@IsDealer bit,
+	@CreatedBy uniqueidentifier,
+	@CreatedDate datetime,
+	@RouteDay nvarchar(500),
+	@ShopName nvarchar(250),
+	@OwnerName nvarchar(250),
+	@OwnerPhone nvarchar(100),
+	@Competitor1TvWalton int,
+	@Competitor1RfWalton int,
+	@Competitor2TvVision int,
+	@Competitor2RfVision int,
+	@Competitor3TvMarcel int,
+	@Competitor3RfMinister int,
+	@MonthlySaleTv float,
+	@MonthlySaleRf float,
+	@MonthlySaleAc float,
+	@ShowroomSize float,
+	@DisplayOut float,
+	@DisplayIn float
+)
+AS
+    INSERT INTO [dbo].[FloatingPoint] 
+	(
+	[FloatingPointId],
+	[Name],
+	[District],
+	[City],
+	[Lat],
+	[Lng],
+	[IsDealer],
+	[CreatedBy],
+	[CreatedDate],
+	[RouteDay],
+	[ShopName],
+	[OwnerName],
+	[OwnerPhone],
+	[Competitor1TvWalton],
+	[Competitor1RfWalton],
+	[Competitor2TvVision],
+	[Competitor2RfVision],
+	[Competitor3TvMarcel],
+	[Competitor3RfMinister],
+	[MonthlySaleTv],
+	[MonthlySaleRf],
+	[MonthlySaleAc],
+	[ShowroomSize],
+	[DisplayOut],
+	[DisplayIn]
+    ) 
+	VALUES 
+	(
+	@FloatingPointId,
+	@Name,
+	@District,
+	@City,
+	@Lat,
+	@Lng,
+	@IsDealer,
+	@CreatedBy,
+	@CreatedDate,
+	@RouteDay,
+	@ShopName,
+	@OwnerName,
+	@OwnerPhone,
+	@Competitor1TvWalton,
+	@Competitor1RfWalton,
+	@Competitor2TvVision,
+	@Competitor2RfVision,
+	@Competitor3TvMarcel,
+	@Competitor3RfMinister,
+	@MonthlySaleTv,
+	@MonthlySaleRf,
+	@MonthlySaleAc,
+	@ShowroomSize,
+	@DisplayOut,
+	@DisplayIn
+    )
+	DECLARE @Err int
+	DECLARE @Result int
+
+	SET @Result = @@ROWCOUNT
+	SET @Err = @@ERROR 
+	If @Err <> 0 
+	BEGIN
+		SET @Id = -1
+		RETURN @Err
+	END
+	ELSE
+	BEGIN
+		If @Result = 1 
+		BEGIN
+			-- Everything is OK
+			SET @Id = @@IDENTITY
+		END
+		ELSE
+		BEGIN
+			SET @Id = -1
+			RETURN 0
+		END
+	END
+
+	RETURN @Id
+GO
+
+/****** Object:  StoredProcedure [dbo].UpdateFloatingPoint    Script Date: 3/12/2021 11:42:24 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateFloatingPoint]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[UpdateFloatingPoint]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE UpdateFloatingPoint
+(
+	@Id int,
+	@FloatingPointId uniqueidentifier,
+	@Name nvarchar(250),
+	@District nvarchar(100),
+	@City nvarchar(100),
+	@Lat nvarchar(100),
+	@Lng nvarchar(100),
+	@IsDealer bit,
+	@CreatedBy uniqueidentifier,
+	@CreatedDate datetime,
+	@RouteDay nvarchar(500),
+	@ShopName nvarchar(250),
+	@OwnerName nvarchar(250),
+	@OwnerPhone nvarchar(100),
+	@Competitor1TvWalton int,
+	@Competitor1RfWalton int,
+	@Competitor2TvVision int,
+	@Competitor2RfVision int,
+	@Competitor3TvMarcel int,
+	@Competitor3RfMinister int,
+	@MonthlySaleTv float,
+	@MonthlySaleRf float,
+	@MonthlySaleAc float,
+	@ShowroomSize float,
+	@DisplayOut float,
+	@DisplayIn float
+)
+AS
+    UPDATE [dbo].[FloatingPoint] 
+	SET
+	[FloatingPointId] = @FloatingPointId,
+	[Name] = @Name,
+	[District] = @District,
+	[City] = @City,
+	[Lat] = @Lat,
+	[Lng] = @Lng,
+	[IsDealer] = @IsDealer,
+	[CreatedBy] = @CreatedBy,
+	[CreatedDate] = @CreatedDate,
+	[RouteDay] = @RouteDay,
+	[ShopName] = @ShopName,
+	[OwnerName] = @OwnerName,
+	[OwnerPhone] = @OwnerPhone,
+	[Competitor1TvWalton] = @Competitor1TvWalton,
+	[Competitor1RfWalton] = @Competitor1RfWalton,
+	[Competitor2TvVision] = @Competitor2TvVision,
+	[Competitor2RfVision] = @Competitor2RfVision,
+	[Competitor3TvMarcel] = @Competitor3TvMarcel,
+	[Competitor3RfMinister] = @Competitor3RfMinister,
+	[MonthlySaleTv] = @MonthlySaleTv,
+	[MonthlySaleRf] = @MonthlySaleRf,
+	[MonthlySaleAc] = @MonthlySaleAc,
+	[ShowroomSize] = @ShowroomSize,
+	[DisplayOut] = @DisplayOut,
+	[DisplayIn] = @DisplayIn
+	WHERE ( Id = @Id )
+
+	DECLARE @Err int
+	DECLARE @Result int
+	SET @Result = @@ROWCOUNT
+	SET @Err = @@ERROR 
+
+	If @Err <> 0 
+	BEGIN
+		SET @Result = -1
+	END
+
+	RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].DeleteFloatingPoint    Script Date: 3/12/2021 11:42:24 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DeleteFloatingPoint]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[DeleteFloatingPoint]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE DeleteFloatingPoint
+(
+	@Id int
+)
+AS
+	DELETE [dbo].[FloatingPoint] 
+
+    WHERE ( Id = @Id )
+
+	DECLARE @Err int
+	DECLARE @Result int
+
+	SET @Result = @@ROWCOUNT
+	SET @Err = @@ERROR 
+
+	If @Err <> 0 
+	BEGIN
+		SET @Result = -1
+	END
+
+	RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].GetAllFloatingPoint    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetAllFloatingPoint]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetAllFloatingPoint]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetAllFloatingPoint
+AS
+	SELECT *		
+	FROM
+		[dbo].[FloatingPoint]
+
+RETURN @@ROWCOUNT
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointById    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointById]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointById]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointById
+(
+	@Id int
+)
+AS
+	SELECT *		
+	FROM
+		[dbo].[FloatingPoint]
+	WHERE ( Id = @Id )
+
+RETURN @@ROWCOUNT
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointMaximumId    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointMaximumId]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointMaximumId]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointMaximumId
+AS
+	DECLARE @Result int
+	SET @Result = 0
+	
+	SELECT @Result = MAX(Id) 		
+	FROM
+		[dbo].[FloatingPoint]
+
+	If @Result > 0 
+		BEGIN
+			-- Everything is OK
+			RETURN @Result
+		END
+		ELSE
+		BEGIN
+			RETURN 0
+		END
+RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointRowCount    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointRowCount]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointRowCount]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointRowCount
+AS
+	DECLARE @Result int
+	SET @Result = 0
+	SELECT @Result = COUNT(*) 		
+	FROM
+		[dbo].[FloatingPoint]
+		
+RETURN @Result
+GO
+
+/****** Object:  StoredProcedure [dbo].GetPagedFloatingPoint    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetPagedFloatingPoint]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetPagedFloatingPoint]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetPagedFloatingPoint
+(
+	@TotalRows		int	OUTPUT,
+	@PageIndex	int,
+	@RowPerPage		int,
+	@WhereClause	nvarchar(4000),
+	@SortColumn		nvarchar(128),
+	@SortOrder		nvarchar(4)
+)
+AS
+BEGIN 
+
+SET @PageIndex = isnull(@PageIndex, -1)
+SET @RowPerPage = isnull(@RowPerPage, -1)
+SET @WhereClause = isnull(@WhereClause, '')
+SET @SortColumn = isnull(@SortColumn, '')
+SET @SortOrder = isnull(@SortOrder, '')
+SET @TotalRows = 0
+SET @RowPerPage = @RowPerPage -1
+DECLARE @SQL1 nvarchar(4000)
+DECLARE @SQL2 nvarchar(4000)
+
+IF (@WhereClause != '')
+BEGIN
+	SET @WhereClause = 'WHERE ' + char(13) + @WhereClause	
+END
+
+IF (@SortColumn != '')
+BEGIN
+	SET @SortColumn = 'ORDER BY ' + @SortColumn
+
+	IF (@SortOrder != '')
+	BEGIN
+		SET @SortColumn = @SortColumn + ' ' + @SortOrder
+	END
+END
+ELSE
+BEGIN
+	SET @SortColumn = @SortColumn + ' ORDER BY [Id] ASC'
+END
+
+SET @SQL1 = 'WITH FloatingPointEntries AS (
+			SELECT ROW_NUMBER() OVER ('+ @SortColumn +')AS Row,
+	[Id],
+	[FloatingPointId],
+	[Name],
+	[District],
+	[City],
+	[Lat],
+	[Lng],
+	[IsDealer],
+	[CreatedBy],
+	[CreatedDate],
+	[RouteDay],
+	[ShopName],
+	[OwnerName],
+	[OwnerPhone],
+	[Competitor1TvWalton],
+	[Competitor1RfWalton],
+	[Competitor2TvVision],
+	[Competitor2RfVision],
+	[Competitor3TvMarcel],
+	[Competitor3RfMinister],
+	[MonthlySaleTv],
+	[MonthlySaleRf],
+	[MonthlySaleAc],
+	[ShowroomSize],
+	[DisplayOut],
+	[DisplayIn]
+				FROM 
+				[dbo].[FloatingPoint]
+					'+ @WhereClause +'
+				)
+				SELECT 
+	[Id],
+	[FloatingPointId],
+	[Name],
+	[District],
+	[City],
+	[Lat],
+	[Lng],
+	[IsDealer],
+	[CreatedBy],
+	[CreatedDate],
+	[RouteDay],
+	[ShopName],
+	[OwnerName],
+	[OwnerPhone],
+	[Competitor1TvWalton],
+	[Competitor1RfWalton],
+	[Competitor2TvVision],
+	[Competitor2RfVision],
+	[Competitor3TvMarcel],
+	[Competitor3RfMinister],
+	[MonthlySaleTv],
+	[MonthlySaleRf],
+	[MonthlySaleAc],
+	[ShowroomSize],
+	[DisplayOut],
+	[DisplayIn]
+				FROM 
+					FloatingPointEntries
+				WHERE 
+					Row between '+ CONVERT(nvarchar(10), (@PageIndex * @RowPerPage) + 1) +'And ('+ CONVERT(nvarchar(10), (@PageIndex * @RowPerPage) +@RowPerPage+ 1) +')'
+	
+
+SET @SQL2 =		' SELECT @TotalRows = COUNT(*) 
+				FROM 
+				[dbo].[FloatingPoint] ' + @WhereClause
+								
+EXEC sp_executesql @SQL2, N'@TotalRows int output', @TotalRows = @TotalRows output
+
+EXEC sp_executesql @SQL1
+
+RETURN @@ROWCOUNT
+END
+GO
+
+/****** Object:  StoredProcedure [dbo].GetFloatingPointByQuery    Script Date: 3/12/2021 11:42:24 AM  ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFloatingPointByQuery]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetFloatingPointByQuery]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE GetFloatingPointByQuery
+(
+	@Query	nvarchar(4000)
+)
+AS
+BEGIN 
+
+SET @Query = isnull(@Query, '')
+DECLARE @SQL1 nvarchar(4000)
+
+IF (@Query != '')
+BEGIN
+	SET @Query = 'WHERE ' + char(13) + @Query	
+END
+
+SET @SQL1 =		'SELECT * 
+				FROM 
+				[dbo].[FloatingPoint] ' + @Query
+								
+EXEC sp_executesql @SQL1
+
+RETURN @@ROWCOUNT
+END
+GO
+
